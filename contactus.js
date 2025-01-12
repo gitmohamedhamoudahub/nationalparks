df14573279dca8549e46d0c3c1d6625fb2077891
gitmohamedhamoudahub <eng.mohamed.hobby@gmail.com>
2025-01-12 15:27:49 -0500

    Adding contact us form

--- contactus.js
+++ contactus.js
@@ -1,3 +1,46 @@
 export function getContactUsPage() {
-    return '<h1>Contact us ya hamada</h1>'
+    const formContainer = document.createElement('div');
+  formContainer.className = 'contactUsContainer';
+  const form = document.createElement('form');
+  form.className = 'contactUsForm';
+  const nameInput = createInputField('Name', 'name');
+  const emailInput = createInputField('Email', 'email', 'email');
+  const messageInput = createInputField('Message', 'message', 'textarea');
+  const submitButton = document.createElement('button');
+  submitButton.type = 'submit';
+  submitButton.textContent = 'Submit';
+  submitButton.className = 'btnSubmit';
+  form.appendChild(nameInput);
+  form.appendChild(emailInput);
+  form.appendChild(messageInput);
+  form.appendChild(submitButton);
+
+
+    return form.innerHTML;
 }
+
+function createInputField(labelText, name, type = 'text') {
+    const fieldContainer = document.createElement('div');
+    fieldContainer.className = 'fieldContainer';
+  
+    const label = document.createElement('label');
+    label.textContent = labelText;
+    label.setAttribute('for', name);
+  
+    let input;
+    if (type === 'textarea') {
+      input = document.createElement('textarea');
+    } else {
+      input = document.createElement('input');
+      input.type = type;
+    }
+    input.name = name;
+    input.id = name;
+    input.required = true;
+  
+    fieldContainer.appendChild(label);
+    fieldContainer.appendChild(input);
+  
+    return fieldContainer;
+  }
+  
