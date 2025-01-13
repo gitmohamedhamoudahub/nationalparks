1238dd16d90481bc1e76aa6e5fb63f7f8ef095a2
gitmohamedhamoudahub <eng.mohamed.hobby@gmail.com>
2025-01-13 05:12:55 -0500

    Style contact us

--- contactus.js
+++ contactus.js
@@ -3,31 +3,34 @@
 export function getContactUsPage() {
 
   console.log('get contact us page');
+  const headerTxt = document.createElement('H1');
+  headerTxt.textContent = 'Contact Us';
+//   contactUsContainer.className = 'contactUsContainer';
   const formContainer = document.createElement('div');
   formContainer.className = 'contactUsContainer';
-//   const form = document.createElement('form');
-//   form.className = 'contactUsForm';
   const nameInput = createInputField('Name', 'name');
   const emailInput = createInputField('Email', 'email', 'email');
   const messageInput = createInputField('Message', 'message', 'textarea');
+  const submitFieldContainer = document.createElement('div');
+  submitFieldContainer.className = 'submitFieldContainer';
   const submitButton = document.createElement('button');
   submitButton.type = 'button';
   submitButton.textContent = 'Submit';
   submitButton.className = 'btnSubmit';
+  submitFieldContainer.appendChild(submitButton);
+  formContainer.appendChild(headerTxt);
   formContainer.appendChild(nameInput);
   formContainer.appendChild(emailInput);
   formContainer.appendChild(messageInput);
-  formContainer.appendChild(submitButton);
-    // Create a container to display submitted data
+  formContainer.appendChild(submitFieldContainer);
+  
+    
     const submittedDataContainer = document.createElement('div');
     submittedDataContainer.className = 'submittedDataContainer';
     submittedDataContainer.innerHTML = '<h3>Submitted Data:</h3>';
-  
-
-//   formContainer.appendChild(form);
-  formContainer.appendChild(submittedDataContainer);
-
-      console.log(formContainer.innerHTML);
+    formContainer.appendChild(submittedDataContainer);
+    // contactUsContainer.appendChild(formContainer);
+    // console.log(contactUsContainer.innerHTML);
     return formContainer.innerHTML;
 }
 
@@ -37,7 +40,7 @@ export function getContactUsPage() {
 
 function createInputField(labelText, name, type = 'text') {
     const fieldContainer = document.createElement('div');
-    fieldContainer.className = 'fieldContainer';
+    fieldContainer.className = 'fieldContainer' + name;
   
     const label = document.createElement('label');
     label.textContent = labelText;
@@ -46,9 +49,11 @@ function createInputField(labelText, name, type = 'text') {
     let input;
     if (type === 'textarea') {
       input = document.createElement('textarea');
+      input.placeholder = labelText;
     } else {
       input = document.createElement('input');
       input.type = type;
+      input.placeholder = labelText;
     }
     input.name = name;
     input.id = name;
